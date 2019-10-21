1716b3b0d997328689c2c5d62ca789a3c30a98e5
Pedja <pedjadjape97@hotmail.rs>
2019-10-21 13:56:03 +0200

    Finish get

--- flask_app/measurement/api.py
+++ flask_app/measurement/api.py
@@ -1,5 +1,6 @@
 from flask import request
 from flask_restplus import Resource
+from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
 
 from flask_app import db
 from flask_app.measurement import measurements_api
@@ -8,10 +9,11 @@ from sqlalchemy.exc import SQLAlchemyError
 
 
 @measurements_api.route('/')
+@measurements_api.route('/<int:measurement_id>')
 class MeasurementApi(Resource):
     def post(self):
         try:
-            data = request.frmo_json(force=True)
+            data = request.get_json(force=True)
 
             measurement = Measurement(
                 temperature=data['temperature'],
@@ -28,4 +30,33 @@ class MeasurementApi(Resource):
             return {'message': 'Unknown error.'}, 500
 
         return {'message': 'Success!'}, 200
+
+    def get(self, measurement_id):
+        try:
+            measurement = db.session. \
+                query(Measurement). \
+                filter(Measurement.id == measurement_id). \
+                one()
+
+        except NoResultFound as error:
+            print(f"No result found for id: {measurement_id}")
+            print(f"Error: {error}")
+            return {'message': 'No result found.'}, 404
+        except MultipleResultsFound as error:
+            print(f"Multiple results found for id: {measurement_id}")
+            print(f"Error: {error}")
+            return {'message': 'Database error.'}, 500
+        except SQLAlchemyError as sqlalchemy_error:
+            print(f"SqlAlchemy error:: {sqlalchemy_error}")
+            return {'message': 'Database error.'}, 500
+        except Exception as server_error:
+            print(f"Server error:: {server_error}")
+            return {'message': 'Server error.'}, 500
+
+        response_data = {'temperature': measurement.temperature,
+                         'air_quality': measurement.air_quality,
+                         'humiidity': measurement.humidity,
+                         'created_at': measurement.created_datetime}
+
+        return response_data, 200
 
